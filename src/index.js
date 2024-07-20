82e2ca6766fb8582a51417fdf64c57796ecb2332
MhmdFais <117585798+MhmdFais@users.noreply.github.com>
2024-07-20 21:57:49 +0530

    style: update design and  layout

--- src/index.js
+++ src/index.js
@@ -2,17 +2,9 @@ import "./style.css";
 import { createHeader } from "./script/header.js";
 
 document.addEventListener("DOMContentLoaded", () => {
+
   createHeader();
 
-  const links = document.querySelectorAll(".navigation ul li a");
-  const currentPage = window.location.pathname;
-
-  links.forEach(link => {
-    // Adjust this condition to match your routing
-    if (link.href.includes(currentPage)) {
-      link.classList.add("active");
-    }
-  });
 });
 
 
