dab8cbd1a057034754155489a5340f43ab863ae4
MhmdFais <117585798+MhmdFais@users.noreply.github.com>
2024-07-22 17:17:45 +0530

    chore: created focus part and the basic logic for timer

--- src/components/timer.js
+++ src/components/timer.js
@@ -1,2 +1,32 @@
 // FOCUS TIME -- 25 MINS
 // BREAK TIME -- 5 MINS
+
+export function showTime(time){
+    const time = document.createElement("p");
+    time.classList.add("time");
+    time.textContent = time;
+    return time;
+}
+
+export function startTimeCountdown(time) {
+    let startingMinutes = time;
+    let timeLeft = startingMinutes * 60;
+
+    const countdownEl = document.querySelector('.time');
+
+    setInterval(() => {
+        updateCountdown(timeLeft, countdownEl);
+        timeLeft--;
+    }, 1000);
+}
+
+function updateCountdown(timeLeft, countdownEl) {
+    const minutes = Math.floor(timeLeft / 60);
+    let seconds = timeLeft % 60;
+
+    seconds = seconds < 10 ? '0' + seconds : seconds;
+
+    countdownEl.innerHTML = `${minutes}:${seconds}`;
+}
+
+
--- src/script/focus.js
+++ src/script/focus.js
@@ -1,1 +1,23 @@
+import {showTime} from '../components/timer'
+
 const focusContainer = document.querySelector(".focus-container");
+
+export function createFocus() {
+    createFunctionHeader();
+}
+
+function createFunctionHeader() {
+    const headerContainer = document.createElement("div");
+    headerContainer.classList.add("sub-headeing-container");
+    const subHeading = createSubHeading()
+    headerContainer.appendChild(subHeading);
+    const time = showTime("25:00");
+    headerContainer.appendChild(time);
+}
+
+function createSubHeading() {
+    const subHeading = document.createElement("p");
+    subHeading.classList.add("sub-heading");
+    subHeading.textContent = "Focus";
+    return subHeading;
+}
