0d79bb51970123726f8fe4190aae449790c7266a
MhmdFais <117585798+MhmdFais@users.noreply.github.com>
2024-07-27 17:14:28 +0530

    style: Update padding in footer and content-container

---
+++ src/script/footer.js
@@ -0,0 +1,37 @@
+const footerElement = document.querySelector("footer")
+
+export function createFooter(){
+    footer()
+}
+
+function footer(){
+    const left = leftSide()
+    const right = rightSide()
+    footerElement.appendChild(left)
+    footerElement.appendChild(right)
+}
+
+function leftSide(){
+    const leftDiv = document.createElement("div")
+    leftDiv.classList.add("left-footer")
+
+    const leftText = document.createElement("p")
+    leftText.textContent = "© 2024 - All Rights Reserved"
+
+    leftDiv.appendChild(leftText)
+
+    return leftDiv
+}
+
+function rightSide(){
+    const rightDiv = document.createElement("div")
+    rightDiv.classList.add("right-footer")
+
+    const rightText = document.createElement("p")
+    rightText.textContent = "Created by: <a href='' target='_blank'>Jorge Sánchez</a>"
+    rightText.innerHTML = rightText.textContent
+
+    rightDiv.appendChild(rightText)
+
+    return rightDiv
+}
